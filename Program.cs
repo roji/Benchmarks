81327445bede9979446925cc87259519968eaa76
Shay Rojansky <roji@roji.org>
2020-03-03 10:21:52 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmark
+{
+    public class Program
+    {
+        [Benchmark]
+        public int Foo()
+        {
+
+        }
+
+        static void Main(string[] args)
+            => BenchmarkRunner.Run<Program>();
+    }
+}
