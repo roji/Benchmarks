7749786669c7238f8c9d28ed1c40ea16ba4a8840
Shay Rojansky <roji@roji.org>
2020-04-13 13:57:11 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+public class Benchmarks
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [Benchmark]
+    public void Foo()
+    {
+    }
+}
