29d825d13bd08bc8602062d76719200521081d3e
Shay Rojansky <roji@roji.org>
2021-10-19 10:37:56 +0200

    Simplify

--- Program.cs
+++ Program.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 BenchmarkRunner.Run<Benchmark>();
 
 [MemoryDiagnoser]
 public class Benchmark
 {
-    private IDbContextFactory<BlogContext> _poolingFactory;
     private BlogContext _context;
 
     private static readonly Func<BlogContext, IAsyncEnumerable<Blog>> _query
@@ -21,36 +18,13 @@ public class Benchmark
     [GlobalSetup]
     public async Task Setup()
     {
-        var services = new ServiceCollection();
-        services.AddPooledDbContextFactory<BlogContext>(options => options
-            .UseNpgsql("Host=localhost;Username=test;Password=test")
-            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
-            .EnableThreadSafetyChecks(false)
-        );
-        var serviceProvider = services.BuildServiceProvider();
-        _poolingFactory = serviceProvider.GetRequiredService<IDbContextFactory<BlogContext>>();
-
-        using (var context = _poolingFactory.CreateDbContext())
+        using (var context = new BlogContext())
         {
             await context.Database.EnsureDeletedAsync();
             await context.Database.EnsureCreatedAsync();
         }
 
-        _context = _poolingFactory.CreateDbContext();
-    }
-
-    // [Benchmark]
-    public async Task<int> QueryWithPooling()
-    {
-        using var ctx = _poolingFactory.CreateDbContext();
-
-        var sum = 0;
-        await foreach (var blog in _query(ctx))
-        {
-            sum += blog.Id;
-        }
-
-        return sum;
+        _context = new BlogContext();
     }
 
     [Benchmark]
@@ -67,9 +41,12 @@ public class Benchmark
 
     public class BlogContext : DbContext
     {
-        public BlogContext(DbContextOptions options) : base(options) {}
-
         public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Username=test;Password=test")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .EnableThreadSafetyChecks(false); // Comment out on EF Core 5.0
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
             => modelBuilder.Entity<Blog>().HasData(new Blog { Id = 1, Title = "EF Core perf is awesome" });
