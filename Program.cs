c8bbe84d0de111a8b539e8a1830d5bf644f8bbc7
Shay Rojansky <roji@roji.org>
2023-03-12 10:26:07 +0100

    Template for PG vs. SQL Server benchmarks

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Data.Common;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using Microsoft.Data.SqlClient;
+using Npgsql;
 
 BenchmarkRunner.Run<Benchmark>();
 
 public class Benchmark
 {
-    [Benchmark]
-    public int Foo()
+    private DbConnection _connection = null!;
+    private DbCommand _command = null!;
+
+    [GlobalSetup(Target = nameof(SqlServer))]
+    public void SqlServerSetup()
     {
+        _connection = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
+        _connection.Open();
+        _command = _connection.CreateCommand();
+        _command.CommandText = "SELECT 1";
+    }
+
+    [GlobalSetup(Target = nameof(PostgreSQL))]
+    public void PostgreSQLSetup()
+    {
+        _connection = new NpgsqlConnection("Host=localhost;Username=test;Password=test;SSL Mode=disable");
+        _connection.Open();
+        _command = _connection.CreateCommand();
+        _command.CommandText = "SELECT 1";
+    }
+
+    [Benchmark]
+    public void SqlServer()
+    {
+        _command.ExecuteNonQuery();
+    }
+
+    [Benchmark]
+    public void PostgreSQL()
+    {
+        _command.ExecuteNonQuery();
     }
 }
